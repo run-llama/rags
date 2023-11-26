aa3bb09fd088a3dc3c752d8b6f8dbacba2df5f07
Alexandros Filothodoros <alexfilothodoros@gmail.com>
2023-11-26 17:42:43 +0100

    fixing some errors

--- agent_utils.py
+++ agent_utils.py
@@ -32,29 +32,21 @@ import uuid
 from constants import AGENT_CACHE_DIR
 import shutil
 
+
 def _resolve_llm(llm_str: str) -> LLM:
     """Resolve LLM."""
-    llm = None  # Initialize llm
-    tokens = llm_str.split(":")
-    if len(tokens) == 1:
-        os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
-        llm = OpenAI(model=llm_str)
-    elif tokens[0] == "local":
-        llm = resolve_llm(llm_str)
-    elif tokens[0] == "openai":
-        os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
-        llm = OpenAI(model=tokens[1])
-    elif tokens[0] == "anthropic":
-        os.environ["ANTHROPIC_API_KEY"] = st.secrets.anthropic_key
-        llm = Anthropic(model=tokens[1])
-    elif tokens[0] == "replicate":
-        os.environ["REPLICATE_API_KEY"] = st.secrets.replicate_key
-        llm = Replicate(model=tokens[1])
-    else:
-        raise ValueError(f"LLM {llm_str} not recognized.")
-    return llm
-
-
+    llm: Optional[LLM] = None  # Initialize llm
+    llm_mapping = {"local": resolve_llm, "openai": OpenAI, "anthropic": Anthropic, "replicate": Replicate}
+    tokens = llm.split(":")     
+    if len(tokens) == 1:        
+        os.environ["OPENAI_API_KEY"] = st.secrets.openai_key         
+        llm = OpenAI(model=llm)     
+    elif tokens[0] in llm_mapping:         
+        os.environ[f"{tokens[0].upper()}_API_KEY"] = st.secrets.get(f"{tokens[0]}_key")         
+        llm = llm_mapping[tokens[0]](model=tokens[1])     
+    else:         
+        raise ValueError(f"LLM {llm} not recognized.")     
+    return llm 
 ####################
 #### META TOOLS ####
 ####################
